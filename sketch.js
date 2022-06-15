fcf6d936a0285f0408643ae5c386c1c7b2cbc0cc
ProfaTatianeTobias <93044628+ProfaTatianeTobias@users.noreply.github.com>
2022-06-15 18:23:49 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,12 +16,20 @@ var bgImg, bunnyImg;
 var link;
 var bunny;
 var button;
+var blinkImg, eatImg, sadImg;
 
 function preload()
 {
   fruitImg = loadImage("assets/melon.png");
   bgImg = loadImage("assets/background.png");
   bunnyImg = loadImage("assets/Rabbit-01.png");
+  blinkImg = loadAnimation("assets/blink_1.png", "assets/blink_2.png", "assets/blink_3.png");
+  eatImg = loadAnimation("assets/eat_0.png", "assets/eat_1.png", "assets/eat_2.png", "assets/eat_3.png", "assets/eat_4.png");
+  sadImg = loadAnimation("assets/sad_1.png", "assets/sad_2.png", "assets/sad_3.png");
+
+  blinkImg.playing = true;
+  blinkImg.looping = true;
+  //eat e sad
 }
 
 function setup() 
@@ -35,9 +43,19 @@ function setup()
   fruit = Bodies.circle(100,100,25);
   World.add(world,fruit);
   link = new Link(rope,fruit);
+
+  blinkImg.frameDelay = 20;
+  //eat e sad
+  
   bunny = createSprite(60,620);
   bunny.addImage(bunnyImg);
-  bunny.scale = 0.22
+  bunny.scale = 0.22;
+
+  bunny.addAnimation('piscando',blinkImg);
+  bunny.addAnimation('comendo',eatImg);
+  //sad
+  bunny.changeAnimation('piscando');
+
   button = createImg("assets/cut_btn.png");
   button.position(25,40);
   button.size(60,60);
